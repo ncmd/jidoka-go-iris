218bf6ef33f5fa37ce8c21879798b5d79b5b8b65
ncmd <cchong.vise@gmail.com>
2018-02-27 22:56:03 -0800

    Deployed Build

--- client/preact.config.js
+++ client/preact.config.js
@@ -1,8 +1,12 @@
 export default config => {
-    config.devServer.proxy = [
-        {
-            path: '/api/**',
-            target: 'http://localhost:8000'
-        }
-    ];
+    if (process.env.NODE_ENV === 'undefined'){
+        config.devServer.proxy = [
+            {
+                path: '/api/**',
+                target: 'http://localhost:8000',
+                type: 'proxy'
+            }
+        ];
+    }
+    console.log(process.env.NODE_ENV);
 };
--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -20,45 +20,45 @@ import {
 } from './types';
 
 // Action Creator, call Express API, uses Dispatch Redux to send to store
-export const fetchUser = () => async dispatch => {
-    const res = await axios.get('/api/current_user');
+export const fetchUser = () => dispatch => {
+    const res = axios.get('/api/current_user');
     dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const handleToken = token => async dispatch => {
-    const res = await axios.post('/api/stripe', token);
+export const handleToken = token => dispatch => {
+    const res = axios.post('/api/stripe', token);
     dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const submitSurvey = (values, history) => async dispatch => {
-    const res = await axios.post('/api/surveys', values);
+export const submitSurvey = (values, history) => dispatch => {
+    const res = axios.post('/api/surveys', values);
     history.push('/surveys');
     dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const fetchSurveys = () => async dispatch => {
-    const res = await axios.get('/api/surveys');
+export const fetchSurveys = () => dispatch => {
+    const res = axios.get('/api/surveys');
     dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
 
-export const fetchRunbooks = () => async dispatch => {
-    const res = await axios.get('/api/runbooks');
+export const fetchRunbooks = () => dispatch => {
+    const res = axios.get('/api/runbooks');
     dispatch({ type: FETCH_RUNBOOKS, payload: res.data });
     console.log("RES DATA:",res.data)
 };
 
-export const fetchSearchRunbook = runbookId => async dispatch => {
-    const res = await axios.get(`/api/runbooks/${runbookId}/search`);
+export const fetchSearchRunbook = runbookId => dispatch => {
+    const res = axios.get(`/api/runbooks/${runbookId}/search`);
     dispatch({ type: FETCH_SEARCH_RUNBOOK, payload: res.data });
 };
 
-export const fetchRunbookComments = runbookId => async dispatch => {
-    const res = await axios.get(`/api/runbooks/${runbookId}/comments`);
+export const fetchRunbookComments = runbookId => dispatch => {
+    const res = axios.get(`/api/runbooks/${runbookId}/comments`);
     dispatch({ type: FETCH_COMMENTS, payload: res.data });
 };
 
-export const submitRunbook = (values, history) => async dispatch => {
-    const res = await axios.post('/api/runbooks', values);
+export const submitRunbook = (values, history) => dispatch => {
+    const res = axios.post('/api/runbooks', values);
     history.push('/');
     dispatch({ type: SUBMIT_RUNBOOK, payload: res.data });
 };
@@ -69,7 +69,7 @@ export const upvoteRunbook = (
     runbookId,
     voteChoice,
     index,
-) => async dispatch => {
+) => dispatch => {
     axios.post(`/api/runbooks/${runbookId}/${voteChoice}`);
     dispatch({ type: UPVOTE_RUNBOOK, index: index });
 };
@@ -77,7 +77,7 @@ export const upvoteRunbook = (
 export const expandRunbookObjective = (
     runbookIndex,
     objectiveIndex,
-) => async dispatch => {
+) => dispatch => {
     console.log('Clicked Expand');
     dispatch({
         type: EXPAND_RUNBOOK_OBJECTIVES,
@@ -90,7 +90,7 @@ export const addComment = (
     runbookId,
     commentAction,
     index,
-) => async dispatch => {
+) => dispatch => {
     console.log('RUNBOOK ID:', runbookId);
     axios.post(`/api/runbooks/${runbookId}/${commentAction}`);
     dispatch({ type: COMMENT_ADD, index: index });
@@ -100,7 +100,7 @@ export const delComment = (
     runbookId,
     commentAction,
     index,
-) => async dispatch => {
+) => dispatch => {
     axios.post(`/api/runbooks/${runbookId}/${commentAction}`);
     dispatch({ type: COMMENT_DEL, index: index });
 };
@@ -109,35 +109,35 @@ export const downvoteRunbook = (
     runbookId,
     voteChoice,
     index,
-) => async dispatch => {
+) => dispatch => {
     axios.post(`/api/runbooks/${runbookId}/${voteChoice}`);
     dispatch({ type: DOWNVOTE_RUNBOOK, index: index });
 };
 
-export const sortRunbooksSumvotes = voteChoice => async dispatch => {
-    const res = await axios.get(`/api/runbooks/filter/${voteChoice}`);
+export const sortRunbooksSumvotes = voteChoice => dispatch => {
+    const res = axios.get(`/api/runbooks/filter/${voteChoice}`);
     console.log(voteChoice);
     dispatch({ type: SORT_RUNBOOKS_SUMVOTES, payload: res.data });
 };
 
-export const selectRunbook = (index, runbookId) => async dispatch => {
-    const res = await axios.get(`/api/runbooks/${runbookId}/details`);
+export const selectRunbook = (index, runbookId) => dispatch => {
+    const res = axios.get(`/api/runbooks/${runbookId}/details`);
     dispatch({ type: SELECT_RUNBOOK, payload: res.data, index: index });
 };
 
-export const loadRunbook = (uri) => async dispatch => {
+export const loadRunbook = (uri) => dispatch => {
     console.log("URI:",uri);
-    const res = await axios.get(`${uri}`);
+    const res = axios.get(`${uri}`);
     dispatch({ type: LOAD_RUNBOOK_FROM_DIRECT_URI, payload: res.data})
-}
+};
 
-export const editRunbook = (index, runbookId) => async dispatch => {
-    const res = await axios.get(`/api/runbooks/${runbookId}/edit`);
+export const editRunbook = (index, runbookId) => dispatch => {
+    const res = axios.get(`/api/runbooks/${runbookId}/edit`);
     dispatch({ type: EDIT_RUNBOOK, payload: res.data, index: index });
 };
 
-export const updateRunbook = (runbookId, index, values, history) => async dispatch => {
-    const res = await axios.post(`/api/runbooks/${runbookId}/update`,values);
+export const updateRunbook = (runbookId, index, values, history) => dispatch => {
+    const res = axios.post(`/api/runbooks/${runbookId}/update`,values);
     console.log("SENDING UPDATE");
     history.push(`/runbooks/${runbookId}/details`);
     dispatch({ type: UPDATE_RUNBOOK, payload: res.data, index: index });
@@ -149,8 +149,8 @@ export const commentRunbook = (
     runbookId,
     comment,
     runbookIndex,
-) => async dispatch => {
-    const res = await axios.post(`/api/runbooks/${runbookId}/comment`, {
+) => dispatch => {
+    const res = axios.post(`/api/runbooks/${runbookId}/comment`, {
         comment: comment,
     });
     dispatch({
